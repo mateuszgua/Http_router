a64efc47a0b43016ef9833b3a435155a32fd0431
Mateusz Gula <mateuszgua@wp.eu>
2022-12-15 15:15:42 +0100

    Feat: add implementation for http router

--- main.go
+++ main.go
@@ -5,13 +5,59 @@ import (
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
-
-	http.ListenAndServe(":8080", mux)
+func indexHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "GET /")
+	})
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+func asdHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			fmt.Fprint(w, "GET /asd")
+		case http.MethodPost:
+			fmt.Fprint(w, "POST / asd")
+		default:
+			fmt.Fprint(w, "Not Found")
+		}
+	})
+}
+
+func asdZxcHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "GET /asd/zxc")
+	})
+}
+
+func asdZxcQweHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "GET /asd/zxc/qwe")
+	})
+}
+
+func zxcHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "GET /zxc")
+	})
+}
+
+func qweHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "GET /qwe")
+	})
+}
+
+func main() {
+
+	r := NewRouter()
+
+	r.Methods(http.MethodGet).Handler(`/`, indexHandler())
+	r.Methods(http.MethodGet, http.MethodPost).Handler(`/asd`, asdHandler())
+	r.Methods(http.MethodGet).Handler(`/asd/zxc`, asdZxcHandler())
+	r.Methods(http.MethodGet).Handler(`/asd/zxc/qwe`, asdZxcQweHandler())
+	r.Methods(http.MethodGet).Handler(`/zxc`, zxcHandler())
+	r.Methods(http.MethodGet).Handler(`/qwe`, qweHandler())
+
+	http.ListenAndServe(":8080", r)
 }
